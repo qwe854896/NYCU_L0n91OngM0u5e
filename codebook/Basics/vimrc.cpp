3fa976a9155cb7b407cd3765166cd25921581f2f
huci0062 <61218291+huroy5518@users.noreply.github.com>
2022-11-19 01:04:24 +0800

    update vimrc although nobody will use vim

--- codebook/Basics/vimrc.cpp
+++ codebook/Basics/vimrc.cpp
@@ -1,7 +1,7 @@
 "This file should be placed at ~/.vimrc"
-se nu ai hls et ru ic is sc cul
-se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
+se nu ai hls et ru ic is sc cul cindent smartindent autoindent
+se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a bg=dark
 syntax on
 hi cursorline cterm=none ctermbg=89
-set bg=dark
-inoremap {<ENTER> {}<LEFT><ENTER><ENTER><UP><TAB>
+inoremap {<ENTER> {<ENTER>}<ESC>O
+autocmd filetype cpp nnoremap<F5> :!g++ % -o ./_ && ./_ <ENTER>
